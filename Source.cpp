cd909a82761c423e95010794b32bb94bba3c9f7a
Mizera-A <mizeraap@acad.sunybroome.edu>
2025-02-26 11:36:39 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	// variable block
+	float totalfarm;
+	string veg1;
+	float veg1_acre;
+	float veg1_cost;
+	float veg1_total;
+	string veg2;
+	float veg2_acre;
+	float veg2_cost;
+	float veg2_total;
+	float total_cost;
+
+	cout << "please enter total number of "
+}
